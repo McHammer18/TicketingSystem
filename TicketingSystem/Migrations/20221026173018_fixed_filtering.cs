b2c912af12838a1a36c860abf31f0e345c7fe21a
McHammer18 <65956506+McHammer18@users.noreply.github.com>
2022-10-26 12:37:20 -0500

    final commit

---
+++ TicketingSystem/Migrations/20221026173018_fixed_filtering.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TicketingSystem.Migrations
+{
+    public partial class fixed_filtering : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
